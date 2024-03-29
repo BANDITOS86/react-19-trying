b8c79e692646310e0b37b5c9a3172fe99455c3af
Alexsandr Valentinov <djvalentinoff@gmail.com>
2024-03-29 12:25:43 +0300

    I got acquainted with the use hook

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { MessageToggler } from './1_use/MessageToggler'
+import { ThemeExample } from './1_use/Theme'
+import { UserInfo } from './1_use/Users'
+import './App.css'
+
+function App() {
+  return (
+   <div>
+     {/* <title>Hello</title>
+      <meta name="description" content="text" /> */}
+
+      {/* <MessageToggler /> */}
+      {/* <UserInfo /> */}
+
+      <ThemeExample />
+   </div>
+  )
+}
+
+export default App
